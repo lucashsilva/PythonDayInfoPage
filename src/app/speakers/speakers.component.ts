fc7e15f75d4f8b1c190e36e5ed8487860931a325
Gustavo Diniz <gustavo.monteiro@ccc.ufcg.edu.br>
2017-03-10 09:13:35 -0300

    Updated information about the event proposal and its speakers.

--- src/app/speakers/speakers.component.ts
+++ src/app/speakers/speakers.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./speakers.component.scss']
 })
 export class SpeakersComponent implements OnInit {
-  speakersPhotos:String[] = ["/assets/img/palestrante_samuel.JPG"];
+  speakersPhotos:String[] = ["/assets/img/palestrante_samuel.JPG", "/assets/img/palestrante_alisson.jpg",
+                            "/assets/img/palestrante_walter.jpg", "/assets/img/palestrante_matheus.jpg"  ];
   constructor() { }
 
   ngOnInit() {
